cefa6efabcec84a56a6c06352a76b530539c4d70
John <jjfloreno@gmail.com>
2018-01-25 16:01:37 -0500

    First commit. Base requirements completed.

---
+++ Lab6/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string EnglishWord;
+            bool RunProgram = true;
+            bool DoAgain;
+
+            while (RunProgram == true)
+            {
+                Console.WriteLine("Hello! Welcome to Lab Number 6!");
+                System.Threading.Thread.Sleep(500);
+
+                //input & convert to lowercase
+                Console.WriteLine("Please enter a word to be translated into Pig Latin:");
+
+                EnglishWord = Console.ReadLine().ToLower();
+                //EnglishWord = Console.ReadLine();
+
+                if ("aeiouAEIOU".Contains(EnglishWord[0])) //if word begins with vowel
+                {
+                    Console.WriteLine(VowelFirst(EnglishWord));
+                }
+                else //if word begins with consonant
+                {
+                    Console.WriteLine(ConsFirst(EnglishWord));
+                }
+
+                DoAgain = false;
+
+                while (DoAgain == false)
+                {
+                    //input to run again
+                    Console.WriteLine("Would you like to translate another word? (Y/N)");
+
+                    string UserResponse = Console.ReadLine();
+
+                    if (String.Compare(UserResponse, "y", true) == 0) //Y -- run again
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Great! Here we go...");
+                        System.Threading.Thread.Sleep(500);
+                        Console.Clear();
+                        DoAgain = true;
+                    }
+                    else if (String.Compare(UserResponse, "n", true) == 0) //N -- quit program
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Goodbye!");
+                        System.Threading.Thread.Sleep(500);
+                        RunProgram = false;
+                        DoAgain = true;
+                    }
+                    else //invalid response
+                    {
+                        Console.WriteLine("Not a valid input.");
+                        System.Threading.Thread.Sleep(500);
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+        public static string VowelFirst(string Word)
+        {
+            return Word + "way";
+        }
+        public static string ConsFirst(string Word)
+        {
+            char[] Vowels = { 'a', 'e', 'i', 'o', 'u', 'A','E','I','O','U' };
+            int VowelIndex = Word.IndexOfAny(Vowels); //finds index of first vowel
+            string Prefix = Word.Substring(0,VowelIndex); //finds the letters up to the first vowel
+            string NewWord = Word.Substring(VowelIndex); //gets the letters after the first vowel
+
+            return NewWord+Prefix+"ay";
+        }
+    }
+}
